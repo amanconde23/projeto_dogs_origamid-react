8bf7898cbda43ebc6e4b8bf7fb092ef107b34c20
amanconde23 <amanconde23@gmail.com>
2022-02-01 08:11:08 -0300

    iniciando rotas de login/cadastro

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginForm from './LoginForm'
+import LoginCreate from './LoginCreate'
+import LoginLost from './LoginLost'
+import LoginReset from './LoginReset'
+
+const Login = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />}/>
+        <Route path="criar" element={<LoginCreate />}/>
+        <Route path="perdeu" element={<LoginLost />}/>
+        <Route path="resetar" element={<LoginReset />}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default Login
